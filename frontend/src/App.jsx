3a361ae4da516e1719044f28de52bf5dc7275b69
varun <coldvks@gmail.com>
2025-09-13 13:25:34 +0530

    frontend: setup vite+react, routing, and login, home pages, and navbar, footer components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import Footer from './components/layout/Footer.jsx'
+import Navbar from './components/layout/Navbar.jsx'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login'
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/courses" element={<h1 className="text-center mt-5">Courses Page</h1>} />
+      </Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
