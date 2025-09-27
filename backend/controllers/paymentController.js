99caa39c33bc614b316bd6aed3f51ec8fe3a45ba
varun <coldvks@gmail.com>
2025-09-27 13:13:45 +0530

    add payment intent

---
+++ backend/controllers/paymentController.js
@@ -0,0 +1,24 @@
+import Stripe from "stripe";
+
+export const createPaymentIntent = async (req, res) => {
+  const { amount } = req.body;
+  try {
+    const args = {
+      amount: amount,
+      currency: "usd",
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    };
+
+    // create payment intent
+    const paymentIntent = await Stripe.paymentIntents.create(args);
+    // get client secret from intent
+    console.log(paymentIntent.client_secret)
+
+    //return client secret to FE
+    return res.status(200).json({clientSecret: paymentIntent.client_secret});
+  } catch (error) {
+    return res.status(500).json({ message: "Server error!" });
+  }
+};
